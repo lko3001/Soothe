ba7efcdccca10c6897039f7f29409d12b8d38a4a
Lko <3001lorenzomaino@gmail.com>
2023-03-07 16:25:53 +0100

    Fixed

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -118,7 +118,7 @@ export default function Editor({ note }: { note: Note }) {
   return (
     <>
       <Head>
-        <title>Note Editor | {id || "Null"}</title>
+        <title>Note Editor</title>
         <meta property="og:title" content="Note Editor" />
         <meta name="description" content="Start Writing your note" />
         <meta property="og:description" content="Start Writing your note" />
@@ -133,8 +133,6 @@ export default function Editor({ note }: { note: Note }) {
       <Toaster />
       <section className="mx-auto max-w-7xl grow p-4 py-8 lg:p-20">
         <div className="mb-4 flex flex-row items-center justify-start gap-4 lg:mb-8">
-          <p>{id || "Null"}</p>
-          {id && <p>Id exists</p>}
           <NavBtn />
           <input
             type="text"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Icon, NavBtn, Navbar, NoteCard } from "@/components";
+import { Icon, Loading, NavBtn, Navbar, NoteCard } from "@/components";
 import { useCallback, useEffect, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { getServerSession } from "next-auth/next";
@@ -42,10 +42,9 @@ export default function Home({
         <meta property="og:image" content="https://i.imgur.com/Q5ZLNOM.png" />
         <meta property="og:url" content="https://soothe.vercel.app/" />
       </Head>
-
-      {notes.length ? (
+      {(notes.length ? (
         <section className="mx-auto max-w-7xl grow p-4 py-8 lg:p-20">
-          <h1 className="text-[rgba(var(--red))]">CIao</h1>
+          <Loading />
           <div className="mb-4 flex flex-row items-center justify-start gap-2 lg:mb-8">
             <NavBtn />
             <input
@@ -103,6 +102,29 @@ export default function Home({
             </Link>
           </section>
         )
+      )) || (
+        <div className="fixed top-0 left-0 z-[100] flex h-screen w-screen items-center justify-center bg-r-white dark:bg-r-darkWhite">
+          <svg
+            className="-ml-1 mr-3 h-8 w-8 animate-spin text-r-accentWarm dark:text-r-darkAccentWarm"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
       )}
     </>
   );
